a3eaee8ffdd59eed873ac674096a0ae2400befa5
Diego Correa Diaz <dacorrea91@ucatolica.edu.co>
2020-10-19 16:09:01 -0500

    Se hizó la barra de navegacion para poder navegar entre las diferentes opciones

---
+++ escort/src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navigation() {
+    return (
+         <nav className="navbar navbar-dark gb-dark">
+        <h1 className="h4">Navegacion</h1>
+        <div className="inline">
+          <a href="" className="registro">Sing Up</a>
+          <a href="" className="inicio">Log In</a>
+        </div>
+      </nav>
+    );
+}
+
+export default Navigation;
